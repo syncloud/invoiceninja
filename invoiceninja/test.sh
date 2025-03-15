6a067125eea2bbe0530e826c20327714872cad36
Boris Rybalkin <ribalkin@gmail.com>
2025-03-15 15:16:29 +0000

    fix

--- invoiceninja/test.sh
+++ invoiceninja/test.sh
@@ -5,5 +5,5 @@ cd ${DIR}
 VERSION=$1
 BUILD_DIR=${DIR}/../build/snap/invoiceninja
 ${BUILD_DIR}/bin/php-fpm.sh --version
-${BUILD_DIR}/bin/php-fpm.sh --version | { ! grep Warning }
+${BUILD_DIR}/bin/php-fpm.sh --version | ( ! grep Warning )
 ${BUILD_DIR}/bin/php.sh --version
