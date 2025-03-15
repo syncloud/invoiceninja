b4644e89f8fee94aa201d13cd075de2eda6e2b14
Boris Rybalkin <ribalkin@gmail.com>
2025-03-15 17:19:18 +0000

    fix sed

--- invoiceninja/test.sh
+++ invoiceninja/test.sh
@@ -5,8 +5,7 @@ cd ${DIR}
 VERSION=$1
 RUNTIME_DIR=/snap/invoiceninja/current/invoiceninja
 BUILD_DIR=${DIR}/../build/snap/invoiceninja
-sed -i "s#$RUNTIME_DIR#$BUILD_DIR#g" ${BUILD_DIR}/usr/local/etc/php
+sed -i "s#$RUNTIME_DIR#$BUILD_DIR#g" ${BUILD_DIR}/usr/local/etc/php/php.ini
 ${BUILD_DIR}/bin/php-fpm.sh --version
 ${BUILD_DIR}/bin/php-fpm.sh --version | ( ! grep Warning )
 ${BUILD_DIR}/bin/php.sh --version
-
