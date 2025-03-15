6f027ac07a23ab9b75597bb7f3ab07310cb1a812
Boris Rybalkin <ribalkin@gmail.com>
2025-03-15 09:50:50 +0000

    php fpm

--- bin/service.php-fpm.sh
+++ bin/service.php-fpm.sh
@@ -2,20 +2,4 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 
-if [[ -z "$1" ]]; then
-    echo "usage $0 [start]"
-    exit 1
-fi
-
-case $1 in
-start)
-    exec $DIR/php/bin/php-fpm.sh -y ${SNAP_DATA}/config/php-fpm.conf -c ${SNAP_DATA}/config/php.ini
-    ;;
-post-start)
-    timeout 5 /bin/bash -c 'until [ -S '${SNAP_COMMON}'/log/php5-fpm.sock ]; do echo "waiting for ${SNAP_COMMON}/log/php5-fpm.sock"; sleep 1; done'
-    ;;
-*)
-    echo "not valid command"
-    exit 1
-    ;;
-esac
+$DIR/invoiceninja/bin/php-fpm.sh -y ${SNAP_DATA}/config/php-fpm.conf -c ${SNAP_DATA}/config/php.ininvoiceninja/bin/php-fpm.sh -y ${SNAP_DATA}/config/php-fpm.conf -c ${SNAP_DATA}/config/php.ini
