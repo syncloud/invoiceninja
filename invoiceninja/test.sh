51281b96bc1034e212d33b3c0ec9c9e98466ef84
Boris Rybalkin <ribalkin@gmail.com>
2025-03-17 09:25:50 +0000

    fix test

--- invoiceninja/test.sh
+++ invoiceninja/test.sh
@@ -3,9 +3,14 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd ${DIR}
 VERSION=$1
-RUNTIME_DIR=/snap/invoiceninja/current/invoiceninja
+
+SNAP=/snap/invoiceninja/current
+SNAP_DATA=/var$SNAP
+RUNTIME_DIR=$SNAP/invoiceninja
 BUILD_DIR=${DIR}/../build/snap/invoiceninja
 sed -i "s#$RUNTIME_DIR#$BUILD_DIR#g" ${BUILD_DIR}/usr/local/etc/php/php.ini
+sed -i "s#$SNAP_DATA#$BUILD_DIR#g" ${BUILD_DIR}/var/www/app/bootstrap/app.php
+
 ${BUILD_DIR}/bin/php-fpm.sh --version
 ${BUILD_DIR}/bin/php-fpm.sh --version | ( ! grep Warning )
 ${BUILD_DIR}/bin/php.sh --version
