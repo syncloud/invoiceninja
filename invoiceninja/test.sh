ec23b4971b151050b771b00f3868f8dc28889703
Boris Rybalkin <ribalkin@gmail.com>
2025-03-18 20:50:55 +0000

    gd test

--- invoiceninja/test.sh
+++ invoiceninja/test.sh
@@ -18,4 +18,6 @@ grep return ${BUILD_DIR}/var/www/app/bootstrap/app.php
 ${BUILD_DIR}/bin/php-fpm.sh --version
 ${BUILD_DIR}/bin/php-fpm.sh --version | ( ! grep Warning )
 ${BUILD_DIR}/bin/php.sh --version
+${BUILD_DIR}/bin/php.sh -i
+${BUILD_DIR}/bin/php.sh -i | grep -i "gd support" | grep -i enabled
 ${BUILD_DIR}/bin/php.sh $BUILD_DIR/var/www/app/artisan key:generate --show
