4f65aae6daf0129f6919bc5dae0210c153554baa
Boris Rybalkin <ribalkin@gmail.com>
2025-03-19 09:32:55 +0000

    fix ext

--- invoiceninja/bin/php-fpm.sh
+++ invoiceninja/bin/php-fpm.sh
@@ -2,5 +2,7 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 LIBS=${DIR}/lib
 LIBS=$LIBS:$(echo ${DIR}/usr/lib)
-LIBS=$LIBS:$(echo ${DIR}/usr/lib/*-linux-gnu*/samba)
-MAGICK_CODER_MODULE_PATH=$(echo ${DIR}/usr/lib/ImageMagickCoders) PHP_INI_SCAN_DIR=${DIR}/usr/local/etc/php ${DIR}/lib/ld-*.so* --library-path $LIBS ${DIR}/usr/local/sbin/php-fpm "$@"
+PHP_INI_SCAN_DIR=${DIR}/usr/local/etc/php:${DIR}/usr/local/etc/php/conf.d \
+  ${DIR}/lib/ld-*.so* \
+  --library-path $LIBS \
+  ${DIR}/usr/local/sbin/php-fpm "$@"
