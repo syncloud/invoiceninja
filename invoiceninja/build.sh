eab79bd3c9244f2c0fecf098757ba0e11ea2f02b
Boris Rybalkin <ribalkin@gmail.com>
2025-03-29 13:27:19 +0000

    fix index

--- invoiceninja/build.sh
+++ invoiceninja/build.sh
@@ -75,5 +75,5 @@ cp invoiceninja-5-stable/app/Libraries/OAuth/OAuth.php ${BUILD_DIR}/var/www/app/
 cp invoiceninja-5-stable/app/Providers/EventServiceProvider.php ${BUILD_DIR}/var/www/app/app/Providers
 cp invoiceninja-5-stable/config/services.php ${BUILD_DIR}/var/www/app/config
 
-cp ${BUILD_DIR}/var/www/app/index.php ${DIR}/../build/snap/web/
+cp ${BUILD_DIR}/var/www/app/public/index.php ${DIR}/../build/snap/web/
 #cp -r ${DIR}/../build/snap/web/* ${BUILD_DIR}/var/www/app/public/
