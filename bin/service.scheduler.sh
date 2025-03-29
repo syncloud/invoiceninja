31d833c389079e26d7b0658181ee96fc86be2ce3
Boris Rybalkin <ribalkin@gmail.com>
2025-03-29 17:52:16 +0000

    fix artisan jobs

--- bin/service.scheduler.sh
+++ bin/service.scheduler.sh
@@ -2,6 +2,7 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 export PHP_BINARY=$DIR/invoiceninja/bin/php.sh
+cd $SNAP/invoiceninja/var/www/app
 $DIR/invoiceninja/bin/php.sh \
   $SNAP/invoiceninja/var/www/app/artisan \
   schedule:work \
--- bin/service.worker.sh
+++ bin/service.worker.sh
@@ -1,5 +1,12 @@
 #!/bin/bash -e
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
-
-$DIR/invoiceninja/bin/php.sh $SNAP/invoiceninja/var/www/app/artisan queue:work --sleep=3 --tries=3 --max-time=3600 --verbose
+export PHP_BINARY=$DIR/invoiceninja/bin/php.sh
+cd $SNAP/invoiceninja/var/www/app
+$DIR/invoiceninja/bin/php.sh \
+  $SNAP/invoiceninja/var/www/app/artisan \
+  queue:work \
+  --sleep=3 \
+  --tries=3 \
+  --max-time=3600 \
+  --verbose
