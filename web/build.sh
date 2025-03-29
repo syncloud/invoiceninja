aef8ae2b91063fae7eb42e3231656291b2a9abcc
Boris Rybalkin <ribalkin@gmail.com>
2025-03-29 00:01:42 +0000

    fix tar

--- web/build.sh
+++ web/build.sh
@@ -7,7 +7,7 @@ BUILD_DIR=${DIR}/../build/snap/web
 cd ${DIR}/../build
 
 wget https://github.com/cyberb/invoiceninja-ui/archive/refs/heads/main.tar.gz
-tar xf invoiceninja-ui-main.tar.gz
+tar xf main.tar.gz
 cd invoiceninja-ui-main
 cp .env.example .env
 set -i 's/VITE_IS_HOSTED=.*/VITE_IS_HOSTED=true/g' .env
