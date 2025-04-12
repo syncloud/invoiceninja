50283a24612f577e5ba62c706053f7c94ac7b378
Boris Rybalkin <ribalkin@gmail.com>
2025-04-12 15:08:35 +0100

    CURL_CA_BUNDLE

--- invoiceninja/bin/php-fpm.sh
+++ invoiceninja/bin/php-fpm.sh
@@ -2,7 +2,8 @@
 DIR=$( cd "$( dirname "$0" )" && cd .. && pwd )
 LIBS=${DIR}/lib
 LIBS=$LIBS:${DIR}/usr/lib
-export PHP_INI_SCAN_DIR=${DIR}/usr/local/etc/php:${DIR}/usr/local/etc/php/conf.d 
+export PHP_INI_SCAN_DIR=${DIR}/usr/local/etc/php:${DIR}/usr/local/etc/php/conf.d
+export CURL_CA_BUNDLE=/var/snap/platform/current/syncloud.ca.crt 
 exec ${DIR}/lib/ld-*.so* \
   --library-path $LIBS \
   ${DIR}/usr/local/sbin/php-fpm "$@"
