74c33df7eb03f4575086a0883f154d5d4b4c4ec5
Boris Rybalkin <ribalkin@gmail.com>
2025-04-30 20:44:31 +0100

    artisan test

--- server/test.sh
+++ server/test.sh
@@ -2,23 +2,19 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd ${DIR}
-VERSION=$1
 
 SNAP=/snap/invoiceninja/current
 SNAP_DATA=/var$SNAP
 BUILD_DIR=${DIR}/../build/snap/server
 TEST_DIR=${DIR}/../build/server.test
 cp -r $BUILD_DIR $TEST_DIR
-BUILD_DIR=$TEST_DIR
 #sed -i "s#$SNAP/invoiceninja#$BUILD_DIR#g" ${BUILD_DIR}/usr/local/etc/php/php.ini
-cd $BUILD_DIR
+cd $TEST_DIR
 mkdir -p framework/{sessions,views,cache}
-sed -i "s#$SNAP_DATA/storage#$BUILD_DIR#g" ${BUILD_DIR}/var/www/app/bootstrap/app.php
-grep return ${BUILD_DIR}/var/www/app/bootstrap/app.php
+sed -i "s#$SNAP_DATA/storage#$TEST_DIR#g" $TEST_DIR/bootstrap/app.php
+grep return $TEST_DIR/bootstrap/app.php
 
-#${BUILD_DIR}/bin/php-fpm.sh --version
-#${BUILD_DIR}/bin/php-fpm.sh --version | ( ! grep Warning )
-#${BUILD_DIR}/bin/php.sh --version
-#${BUILD_DIR}/bin/php.sh -i
-#${BUILD_DIR}/bin/php.sh -i | grep -i "gd support" | grep -i enabled
-${BUILD_DIR}/bin/artisan.sh key:generate --show
+#$TEST_DIR/bin/artisan.sh key:generate --show
+
+export PATH=$PATH:$BUILD_DIR/../php/bin
+$BUILD_DIR/../php/bin/php.sh artisan key:generate --show
