ce425a48c032431ebdf34be00366a5483798d9aa
Boris Rybalkin <ribalkin@gmail.com>
2025-05-02 22:59:20 +0100

    debug artisan

--- bin/php.sh
+++ bin/php.sh
@@ -1,3 +1,4 @@
 #!/bin/bash -e
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
+grep extension_dir /var/snap/invoiceninja/current/config/php.ini
 exec $DIR/php/bin/php.sh -c /var/snap/invoiceninja/current/config/php.ini "$@"
