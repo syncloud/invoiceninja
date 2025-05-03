154ddd81b4f003ad05332208aab3148bc1b9d290
Boris Rybalkin <ribalkin@gmail.com>
2025-05-03 21:42:34 +0100

    fix php ini

--- bin/php.sh
+++ bin/php.sh
@@ -1,4 +1,3 @@
 #!/bin/bash -e
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
-grep extension_dir /var/snap/invoiceninja/current/config/php.ini
-exec $DIR/php/bin/php.sh -c /var/snap/invoiceninja/current/config/php.ini "$@"
+exec $DIR/php/bin/php.sh -c /snap/invoiceninja/current/config/php.ini "$@"
