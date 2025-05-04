ff83915a8ad0ec190b958450d620f1d644a65348
Boris Rybalkin <ribalkin@gmail.com>
2025-05-04 13:10:41 +0100

    fix scheduler php

--- bin/service.worker.sh
+++ bin/service.worker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
-export PHP_BINARY=$DIR/php/bin/php.sh
+export PHP_BINARY=$DIR/bin/php.sh
 cd $SNAP/server
 exec $DIR/bin/artisan.sh \
   queue:work \
