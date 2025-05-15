d0f8284b3a0f3b35f748a7f883c1dd2bad3398e6
Boris Rybalkin <ribalkin@gmail.com>
2025-05-15 19:24:47 +0100

    pulseaudio

--- php/bin/chromium.sh
+++ php/bin/chromium.sh
@@ -3,6 +3,7 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 LIBS=$(echo ${DIR}/lib/*-linux-gnu*)
 LIBS=$LIBS:$(echo ${DIR}/usr/lib/*-linux-gnu*)
 LIBS=$LIBS:$(echo ${DIR}/usr/lib)
+LIBS=$LIBS:$(echo ${DIR}/usr/lib/*-linux-gnu*/pulseaudio)
 exec ${DIR}/lib/*-linux*/ld-*.so \
   --library-path $LIBS \
   ${DIR}/usr/lib/chromium/chromium "$@"
