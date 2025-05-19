abc7c1d47871b7cee10c7ebd848088cf54da77f2
Boris Rybalkin <ribalkin@gmail.com>
2025-05-19 23:28:55 +0100

    fix playwright dir

--- chromium/build.sh
+++ chromium/build.sh
@@ -7,7 +7,7 @@ mkdir -p $BUILD_DIR
 cp -r /usr ${BUILD_DIR}
 cp -r /lib ${BUILD_DIR}
 cp -r /etc ${BUILD_DIR}
+cp -r /ms-playwright/chromium-*/chrome-linux/* ${BUILD_DIR}/lib/*-linux-gnu*/
 cp -r ${DIR}/bin ${BUILD_DIR}/bin
-cp -r ${DIR}/ms-playwright/chromium-*/chrome-linux/* ${BUILD_DIR}/lib/*-linux-gnu*/
 
 sed -i 's#<dir>/usr/share/fonts</dir>#<dir>/snap/invoiceninja/current/chromium/usr/share/fonts</dir>#g' ${BUILD_DIR}/etc/fonts/fonts.conf
