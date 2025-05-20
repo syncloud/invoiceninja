b6ab9aac5629946d42cf416daef4ab25ee5f096a
Boris Rybalkin <ribalkin@gmail.com>
2025-05-20 08:55:02 +0100

    logger

--- chromium/bin/chromium.sh
+++ chromium/bin/chromium.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash -xe
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 
 export FONTCONFIG_PATH=$DIR/etc/fonts
@@ -7,6 +7,12 @@ LIBS_DIR=$(echo ${DIR}/lib/*-linux-gnu*)
 LIBS=$LIBS_DIR:$(echo ${DIR}/usr/lib/*-linux-gnu*)
 LIBS=$LIBS:$(echo ${DIR}/usr/lib)
 LIBS=$LIBS:$(echo ${DIR}/usr/lib/*-linux-gnu*/pulseaudio)
+
+logger "${LIBS_DIR}/ld-*.so* \
+  --library-path $LIBS \
+  ${LIBS_DIR}/chrome $@ --single-process --headless=old"
+
+
 exec ${LIBS_DIR}/ld-*.so* \
   --library-path $LIBS \
   ${LIBS_DIR}/chrome "$@" --single-process --headless=old
