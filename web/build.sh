47a0b67b73681b82e3a3ea90ab06a775aee7c6eb
Boris Rybalkin <ribalkin@gmail.com>
2025-05-24 12:09:36 +0100

    username

--- web/build.sh
+++ web/build.sh
@@ -13,11 +13,15 @@ tar xf $VERSION.tar.gz
 mv ui-$VERSION web
 
 cd ${BUILD_DIR}/web
+
+sed -i 's/email_address/username/g' src/pages/authentication/Login.tsx
+sed -i 's/email/username/g' src/pages/authentication/Login.tsx
+
 cp .env.example .env
 sed -i 's/VITE_IS_HOSTED=.*/VITE_IS_HOSTED=false/g' .env
 sed -i 's/VITE_IS_TEST=.*/VITE_IS_TEST=false/' .env
 cp ${SERVER_DIR}/vite.config.ts.react vite.config.js
-sed -i '/"version"/c\  "version": " $VERSION - '`date +%Y.%m.%d`'",' package.json
+#sed -i '/"version"/c\  "version": " $VERSION",' package.json
 
 npm config set fetch-retry-mintimeout 200000
 npm config set fetch-retry-maxtimeout 1200000
